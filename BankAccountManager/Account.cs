74476f2396782e98c4b8681baf1d22f026e8bf9e
RasmusAgergaard <rasmusap@gmail.com>
2019-02-25 15:24:20 +0100

    Project created

---
+++ BankAccountManager/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManager
+{
+    public abstract class Account
+    {
+        private string _name;
+        private float _balance;
+
+        
+        public Account()
+        {
+
+        }
+
+        public void InitAccount(string ownerName, float startBalance)
+        {
+            _name = ownerName;
+            _balance = startBalance;
+        }
+
+        public string ReturnName()
+        {
+            return _name;
+        }
+
+        public float ReturnBalance()
+        {
+            return _balance;
+        }
+        
+    }
+}
---
+++ BankAccountManager/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccountManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CheckingAccount checkingAccount = new CheckingAccount();
+
+            checkingAccount.InitAccount("Morten", 0);
+
+            label1.Text = checkingAccount.ReturnName();
+        }
+    }
+}
